10cf591ed55122fd54445da0b0030662a71ea0f7
Sushil Dhore <sushildhore26@gmail.com>
2023-08-22 17:46:44 +0530

    Modified

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+#Installing Dependendes On Server
+sudo apt-get update -y
+
+
+#Installing docker on server
+sudo apt install docker.io -y
+
+sudo systemctl restart docker
+
+sudo systemctl enable docker 
+
+
 
 #Pull the Docker-images from Docker-hub
 docker pull sushil26/simple-python-flask-app:latest
